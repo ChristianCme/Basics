939feb0605d948e1390d6a32f01b03ffd6586c70
ChristianCme <cmehil.warn@gmail.com>
2018-03-09 12:33:38 -0600

    Initial commit

---
+++ Basics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basics
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
